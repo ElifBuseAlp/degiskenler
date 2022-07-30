21b80236fb2f81acedfeefb18d46dffa166e7f97
elif buse alp <elifbusealp@gmail.com>
2022-07-30 12:15:30 +0300

    degiskenler commit

--- Program.cs
+++ Program.cs
@@ -2,8 +2,86 @@
 
 
 int deger = 8;
+string degisken = " ";
 
-Console.WriteLine(deger);
+//Console.WriteLine(deger);
+
+byte b = 5; //0-255  1 byte
+
+sbyte c = 5;//-128 .. 127 1 byte
+
+short d = 5; //2 byte
+
+ushort us = 5; // 2 byte eksi değer alamaz
+
+Int16 e = 2; // 2 byte
+
+int f = 5; //4 byte
+
+Int32 g = 8; //8 byte
+
+long l = 4; //8 byte
+ulong k = 4; //8 byte eksi değer alamaz
+
+float flo = 8; // 4 byte
+
+double dob = 6; //8 byte
+
+decimal dec = 5; // 16 byte
+
+char ch = 'a'; //2 byte
+
+string  str = "buse"; //sınırsız
+
+bool b1 = true;
+bool b2 = false;
+/*
+DateTime dt = DateTime.Now;
+Console.WriteLine(dt);
+Console.ReadLine();
+*/
+
+object o1 = "x";
+object o2 = 'c';
+object o3 = 3;
+object o4 = 4.6;
+
+string str2 = "";
+string str3 = string.Empty;
+str3 = "buse";
+
+string ad = "buse";
+string soyad = "alp";
+
+int i1 = 3;
+int i2 = 6;
+int i3 = i1 + i2 ; 
+
+bool bo1 = 10>2; //true verir
+
+//Console.WriteLine(bo1);
+
+string za = "20";
+int intt = 20;
+
+string yenideger = za + intt.ToString();  //int to string
+
+int da = Convert.ToInt32(za);  //string to int
+
+int par = int.Parse(za);  //string to int
+
+
+
+
+
+
+
+
+
+
+
+
+
 Console.ReadLine();
 
 
